71d8a9e950ea7e2377158a8f688765f68e4fc1db
Jithin <jithinbabu657@gmail.com>
2021-02-11 21:41:52 +0900

    First commit

---
+++ src/main/java/com/serverless/SpringCloudFunctionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFunctionExampleApplication.class, args);
+    }
+
+}
